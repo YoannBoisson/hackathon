7977e299fae13df8a676d7be95c6f35eaa71e0ba
Jimmy <rosko08@protonmail.com>
2021-11-25 18:08:22 +0100

    positionnement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Searchbar from "./components/SearchBar/Searchbar";
 import Navbar from "./components/Navbar";
 import Router from "./routes";
+import ThemeSwitcher from "./contexts/ThemeSwitcher";
 
 function App() {
   return (
     <div className="app-container">
+      <ThemeSwitcher />
       <Navbar />
-      <Router />
       <Searchbar />
+      <Router />
     </div>
   );
 }
