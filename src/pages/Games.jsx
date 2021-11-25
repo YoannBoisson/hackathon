f2893d2b64741ac4d77dad833596e89ef079af54
bkaneb <bryankaneb2@gmail.com>
2021-11-26 00:42:09 +0100

    pull navbar

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import PagesAffContext from '../contexts/PagesAffContext';
 
 const Games = () => {
+  const { setCurrentAffConnexion, setCurrentAffInscription } = useContext(PagesAffContext);
+  setCurrentAffConnexion(false);
+  setCurrentAffInscription(false);
   return <h1>Bienvenue sur la page de jeux</h1>;
 };
 
